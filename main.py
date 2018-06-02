cd5192294df7889f0bacf17daf5f5e5b0f719008
FoxyByte <metalremol@wp.pl>
2018-06-02 12:15:49 +0200

    main sw

---
+++ main.py
@@ -0,0 +1,24 @@
+wyniki = open("el2.txt", "r")
+lines = wyniki.readlines()
+daty = []
+liczby = []
+wartosc = []
+losowanie = []
+for line in lines:
+    losowanie = (line.split())
+    daty.append(losowanie[1])
+    liczby.append(losowanie[2].split(","))
+wyniki.close()
+for wynik in liczby:
+    wynik = [int(i) for i in wynik]
+    wartosc.append(wynik)
+Mini = dict(zip(daty, wartosc))
+print(Mini)
+
+def licznik(i):
+    return print(Mini[i])
+    for i in Mini:
+
+
+licznik("07.03.1979")
+
